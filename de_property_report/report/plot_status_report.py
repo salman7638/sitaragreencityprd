61fd50856bc940b9aeb8962b80722fc03664f941
salman7638 <79008117+salman7638@users.noreply.github.com>
2022-05-24 14:40:39 +0500

    Update plot_status_report.py

--- de_property_report/report/plot_status_report.py
+++ de_property_report/report/plot_status_report.py
@@ -220,7 +220,7 @@ class PlotStatusXlS(models.AbstractModel):
             sheet.write(row, 10, '{0:,}'.format(int(round(phase_grand_booked_total_number_of_plots))), header_row_style)
             sheet.write(row, 11, round(phase_grand_booked_total_number_of_marlas,2), header_row_style)
             sheet.write(row, 12, '{0:,}'.format(int(round(phase_grand_sold_total_number_of_plots))), header_row_style)
-            sheet.write(row, 13, round(phase_grand_all_sold_total_number_of_marlas,2), header_row_style)
+            sheet.write(row, 13, round(phase_grand_sold_total_number_of_marlas,2), header_row_style)
             sheet.write(row, 14, '{0:,}'.format(int(round(phase_grand_all_sold_total_number_of_plots))), header_row_style)
             sheet.write(row, 15, round(phase_grand_all_sold_total_number_of_marlas,2), header_row_style)
             row += 1    
